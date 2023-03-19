c9a5cd46c8aad327a9e871be17c10b1e866d9efb
revachol23 <bablosc@gmail.com>
2023-03-19 18:57:05 -0300

    Made scripting, modifier function

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 const main = document.getElementById("main");
+const button = document.getElementsByClassName('canvasBtn')[0]
 
+function gridGen(){
 for(let i = 0; i < 256; i++){
 let square = document.createElement("div")
 square.classList.add("grid")
 main.appendChild(square);
+square.addEventListener('mouseover',(e) => e.target.style.background = 'black')
+}}
+
+gridGen()
+
+function gridMod(gridNums){
+while (main.firstChild){
+    main.removeChild(main.firstChild)
 }
+for(let i = 0; i < gridNums; i++){
+    let square = document.createElement("div")
+    square.classList.add("grid")
+    main.appendChild(square);
+    square.addEventListener('mouseover',(e) => e.target.style.background = 'black')
+    }
+}
+
+button.addEventListener('click', function(){
+    let numInfo = prompt('Number of squares per side for the new grid?');
+    gridMod(numInfo** 2)
+})
