04962a40031c581842e4d276641c59e4e4e234d0
revachol23 <bablosc@gmail.com>
2023-03-19 19:24:16 -0300

    fully implemented canvas changing button

--- script.js
+++ script.js
@@ -15,15 +15,18 @@ function gridMod(gridNums){
 while (main.firstChild){
     main.removeChild(main.firstChild)
 }
-for(let i = 0; i < gridNums; i++){
+for(let i = 0; i < gridNums** 2; i++){
     let square = document.createElement("div")
     square.classList.add("grid")
     main.appendChild(square);
+    main.setAttribute('style', `grid-template-columns:repeat(${gridNums}, 1fr); grid-template-rows:repeat(${gridNums}, 5vw)` )
     square.addEventListener('mouseover',(e) => e.target.style.background = 'black')
     }
 }
 
 button.addEventListener('click', function(){
     let numInfo = prompt('Number of squares per side for the new grid?');
-    gridMod(numInfo** 2)
+    if (numInfo < 100){
+    gridMod(numInfo)}
+    else alert('Use a lower number.')
 })
